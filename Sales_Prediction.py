4b1b57dce2bdacfeceed7937796f581a7f073d13
Kaharau Tuoro <79953709+AvocadoTurtlez@users.noreply.github.com>
2022-09-13 19:37:44 +1200

    Add files via upload

--- Sales_Prediction.py
+++ Sales_Prediction.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow.python.keras
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
@@ -13,31 +14,38 @@ print(data)
 sns.pairplot(data)
 plt.show(block=True)
 
-inputs = data.drop(['Customer_Name', 'Customer_Email', 'Country', 'Gender', 'Age', 'Annual_Salary', 'Credit_Card_Debt', 'Net_Worth','Purchase_Amount'], axis = 1)
-print(inputs)
-print("Input data Shape=",inputs.shape)
+X = data.drop(['Customer_Name', 'Customer_Email', 'Country', 'Purchase_Amount'], axis = 1)
+print(X)
+print("X data Shape=",X.shape)
 
-output = data['Purchase_Amount']
-print(output)
-output = output.values.reshape(-1,1)
-print("Output Data Shape=",output.shape)
+Y = data['Purchase_Amount']
+print(Y)
+Y = Y.values.reshape(-1,1)
+print("Y Data Shape=",Y.shape)
 
 scaler_in = MinMaxScaler()
-input_scaled = scaler_in.fit_transform(inputs)
-print(input_scaled)
+X_scaled = scaler_in.fit_transform(X)
+print(X_scaled)
 
 scaler_out = MinMaxScaler()
-output_scaled = scaler_out.fit_transform(output)
-print(output_scaled)
+Y_scaled = scaler_out.fit_transform(Y)
+print(Y_scaled)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X_scaled, Y_scaled, test_size=0.2)
+hl=25
 
 model = Sequential()
-model.add(Dense(25, input_dim=5, activation='relu'))
-model.add(Dense(25, activation='relu'))
+model.add(Dense(hl, input_dim=5, activation='relu'))
+model.add(Dense(hl, activation='relu'))
 model.add(Dense(1, activation='linear'))
 print(model.summary())
 
-model.compile(optimizer = 'adam', loss = 'mean_squared_error')
-epochs_hist = model.fit(input_scaled, output_scaled, epochs=20, batch_size=10, verbose=1, validation_split=0.2)
+
+
+model.compile(optimizer='adam', loss = 'mean_squared_error', metrics=['accuracy'])
+epochs_hist = model.fit(X_train, Y_train, epochs=40,
+                        batch_size=64, verbose=1,
+                        validation_data=(X_test, Y_test))
 print(epochs_hist.history.keys())
 
 plt.plot(epochs_hist.history['loss'])
@@ -49,9 +57,9 @@ plt.xlabel('Epoch Number')
 plt.legend(['Training Loss', 'Validation Loss'])
 plt.show()
 
-input_test_sample = np.array([[0, 41.8,  62812.09, 11609.38, 238961.25]])
-input_test_sample_scaled = scaler_in.transform(input_test_sample)
-output_predict_sample_scaled = model.predict(input_test_sample_scaled)
-print('Predicted Output (Scaled) =', output_predict_sample_scaled)
-output_predict_sample = scaler_out.inverse_transform(output_predict_sample_scaled)
-print('Predicted Output / Purchase Amount ', output_predict_sample)
+X_test_sample = np.array([[0, 41.8,  62812.09, 11609.38, 238961.25]])
+X_test_sample_scaled = scaler_in.transform(X_test_sample)
+Y_predict_sample_scaled = model.predict(X_test_sample_scaled)
+print('Predicted Y (Scaled) =', Y_predict_sample_scaled)
+Y_predict_sample = scaler_out.inverse_transform(Y_predict_sample_scaled)
+print('Predicted Y / Purchase Amount ', Y_predict_sample)
